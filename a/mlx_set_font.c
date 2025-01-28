ddd9297293b31597e4884b48e1c1fd4d24236063
Soufiane El Bourakkadi <souel-bo@e2r8p4.1337.ma>
2025-01-28 21:52:37 +0100

    another test

---
+++ a/mlx_set_font.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mlx_set_font.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: souel-bo <souel-bo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/30 13:30:47 by amalliar          #+#    #+#             */
+/*   Updated: 2025/01/27 17:00:40 by souel-bo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mlx_int.h"
+
+/*
+** Allows to specify the font that will be used by mlx_string_put.
+**
+** Note: only fixed-width bitt_game fonts are supported by Xlib, refer to xfontsel
+** utility to get valid font names for this function.
+*/
+
+void	mlx_set_font(t_xvar *xvar, t_win_list *win, char *name)
+{
+	static Font		font = 0;
+
+	if (font)
+		XUnloadFont(xvar->display, font);
+	font = XLoadFont(xvar->display, name);
+	XSetFont(xvar->display, win->gc, font);
+}
